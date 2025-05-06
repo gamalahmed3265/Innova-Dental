d559cfb1017c20e96c81fc8559e09145166faf1d
= <=>
2025-05-06 15:29:23 +0300

    add pages

---
+++ assets/scripts/swiper.js
@@ -0,0 +1,11 @@
+// Initialize Swiper
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: true,
+  },
+  loop: true,
+  speed: 500,
+});
